60d798c1396bab94db5d602a08588cd00fc70e5a
eladmeidar <elad@eizesus.com>
2013-04-08 14:20:41 +0300

    [INIT] initial script

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def who
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holo::Application.config.secret_token = '927380951cf67374c11c0770b017938876ca3ee9f0d708d6af92286cb66fb6aa8b9ae790a3a17e985f6dcbb511d0e82d0bdf070e35d06202be757e12e9dfc1d8'
